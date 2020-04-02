af76cda8b9fe08921fb6b237c55d05eb4e6e2ca0
Roberto Higor <roberto.higor@outlook.com>
2020-04-02 15:58:29 -0300

    Ler propriedade de arquivo

---
+++ Spring Demo1/src/br/com/ilovecode/springdemo/FortuneXML.java
@@ -0,0 +1,12 @@
+package br.com.ilovecode.springdemo;
+
+public class FortuneXML implements FortuneService {
+
+	String[] fortunes;
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

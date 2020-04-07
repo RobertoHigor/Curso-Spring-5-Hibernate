11789a788bb9f0f35b4972526ac68bf4ac2a5df5
Roberto Higor <roberto.higor@outlook.com>
2020-04-07 15:12:47 -0300

    Java Config Class

---
+++ Spring demo Annotation/src/com/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		// Pegando o bean com id tennisCoach definido pela annotation 
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		context.close();
+
+	}
+
+}
---
+++ Spring demo Annotation/src/com/springdemo/SwimCoach.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+public class SwimCoach implements Coach{
+	FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Swim Coach";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return null;
+	}
+
+}

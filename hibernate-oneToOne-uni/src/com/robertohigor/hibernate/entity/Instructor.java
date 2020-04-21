fd8386c37800603f8e889408a5559b6d875e6f09
Roberto Higor <roberto.higor@outlook.com>
2020-04-21 16:38:28 -0300

    Criado projeto para mapeamento relacional OneToOne

---
+++ hibernate-oneToOne-uni/src/com/robertohigor/hibernate/entity/Instructor.java
@@ -0,0 +1,86 @@
+package com.robertohigor.hibernate.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Instructor {
+
+	@Id
+	@Column
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column
+	private String email;
+	
+	@OneToOne(cascade=CascadeType.ALL) // Relação One to One com Cascade
+	@JoinColumn(name="instructor_detail_id") // O nome da chave estrangeira
+	private int instructorDetail;
+	
+	public Instructor() {}
+
+	public Instructor(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(int instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	
+}

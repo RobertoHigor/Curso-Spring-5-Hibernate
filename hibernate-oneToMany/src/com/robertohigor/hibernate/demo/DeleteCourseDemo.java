7dfe2576a1658f175a711d09b759fafbd51a1766
Roberto Higor <roberto.higor@outlook.com>
2020-04-24 18:12:31 -0300

    GET Instrutores e cursos associados. Remover apenas o Curso.

--- hibernate-oneToMany/src/com/robertohigor/hibernate/demo/DeleteCourseDemo.java
+++ hibernate-oneToMany/src/com/robertohigor/hibernate/demo/DeleteCourseDemo.java
@@ -8,7 +8,7 @@ import com.robertohigor.hibernate.entity.Course;
 import com.robertohigor.hibernate.entity.Instructor;
 import com.robertohigor.hibernate.entity.InstructorDetail;
 
-public class GetInstructorCoursesDemo {
+public class DeleteCourseDemo {
 	public static void main (String[] args) {
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml") // O nome é opcional
@@ -23,14 +23,13 @@ public class GetInstructorCoursesDemo {
 		// Iniciando a transação
 			session.beginTransaction();	
 			
-		// Retornar um Instrutor e seus cursos
-			int theId = 1;
-			Instructor tempInstructor = session.get(Instructor.class, theId);
+		// Retornar um curso
+			int theId = 10;
+			Course tempCourse = session.get(Course.class, theId);
 			
-			System.out.println("Instructor: " + tempInstructor);
+			// Por conta do CascadeType, irá remover apenas o curso.
+			session.delete(tempCourse);
 			
-			// Cursos do Instrutor
-			System.out.println("Courses: " + tempInstructor.getCourses());
 			
 		// Finalizando a transação
 			

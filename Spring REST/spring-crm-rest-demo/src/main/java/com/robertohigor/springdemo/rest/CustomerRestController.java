52dbbce6444c686b055c412df248ebc22ccdd67b
Roberto Higor <roberto.higor@outlook.com>
2020-11-06 20:00:06 -0300

    Adicionado CREATE/UPDATE/DELETE customer com REST

--- Spring REST/spring-crm-rest-demo/src/main/java/com/robertohigor/springdemo/rest/CustomerRestController.java
+++ Spring REST/spring-crm-rest-demo/src/main/java/com/robertohigor/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.robertohigor.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +36,34 @@ public class CustomerRestController {
 		}
 		return theCustomer;
 		//return customerService.getCustomer(customerId);
+	}
+	
+	// Adicionar customer - POST /customers
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		// Forçar um insert caso enviem um id
+		// Id 0 é o mesmo que vazio. O método saveCustomer está utilizando o saveOrUpdate()
+		theCustomer.setId(0);
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	// Atualizer customer - PUT /customers
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	// Remover customer - DELETE /customers
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		// Checar se existe
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		if (tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		customerService.deleteCustomer(customerId);
+		return "Deleted customer id - " + customerId;
 	}
 }

df61b685326c14ecd28b8c115c3bbd4c3317fef7
Roberto Higor <roberto.higor@outlook.com>
2020-11-21 16:11:34 -0300

    Projetos com Spring Data JPA e Spring Data REST que eliminam código REST, DAO e Service

--- Spring Boot/springbootrestcrud/src/main/java/com/robertohigor/springbootrestcrud/dao/EmployeeDAO.java
+++ Spring Boot/springbootrestcrud/src/main/java/com/robertohigor/springbootrestcrud/dao/EmployeeDAO.java
@@ -3,7 +3,12 @@ package com.robertohigor.springbootrestcrud.dao;
 import java.util.List;
 
 import com.robertohigor.springbootrestcrud.entity.Employee;
-
+/*
+ * Existem 3 versões do DAO
+ * 1. Com Hibernate nativo na classe EmployeeDAOHibernateImpl
+ * 2. Com a API padrão JPA na classe EmployeeDAOJpaImpl, com a vantagem de facilitar a troca de implementações JPA
+ * 3. Utilizando Spring Data JPA, não necessitando de implementação
+ */
 public interface EmployeeDAO {
 
 	// Listar todos

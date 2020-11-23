613598804b3836291bf4b3c2311f6fc97f050643
Roberto Higor <roberto.higor@outlook.com>
2020-11-23 20:17:28 -0300

    Create com Thymeleaf

--- 7-Spring Boot + Thymeleaf/1-thymeleaf-crud/src/main/java/com/robertohigor/thymeleafdemo/controller/EmployeeController.java
+++ 7-Spring Boot + Thymeleaf/1-thymeleaf-crud/src/main/java/com/robertohigor/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.robertohigor.thymeleafdemo.entity.Employee;
@@ -28,6 +30,21 @@ public class EmployeeController {
 		
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";						  
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		// Criar o atributo model para realizar um "bind" nos dados do formulário
+		Employee theEmployee = new Employee();
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+	    employeeService.save(theEmployee);
+	    return "redirect:/employees/list";
 	}
 }
--- 7-Spring Boot + Thymeleaf/1-thymeleaf-crud/src/main/java/com/robertohigor/thymeleafdemo/dao/EmployeeRepository.java
+++ 7-Spring Boot + Thymeleaf/1-thymeleaf-crud/src/main/java/com/robertohigor/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,11 +1,15 @@
 package com.robertohigor.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.robertohigor.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	// that's it ... no need to write any code LOL!
+	// Método customizado para ordenar por nome
+	// De acordo com o nome do método, o Spring JPA irá criar a query apropriada
+	public List<Employee> findAllByOrderByLastNameAsc();
 	
 }
